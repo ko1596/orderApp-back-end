11b8b3412eeb963c2c17f3e345f46a951e9c5900
107590006 <t107590006@ntut.org.tw>
2021-04-09 00:45:55 +0800

    initial commit

---
+++ src/main/java/org/ntutosl/order_system_backend/OrderSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ntutosl.order_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ntutosl/order_system_backend/OrderSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ntutosl.order_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
